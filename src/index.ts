b09367bcb69ed49b107d7787e3ee661e14268748
tasddc1226 <tasddc@naver.com>
2021-07-24 00:15:25 +0900

    #7 Blockchain Creating a Block 3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-// Typescript Part 6
-// Blockchain Creating a Block 2
+// Typescript Part 7
+// Blockchain Creating a Block 3
 
 // import crypto-js
 import * as CryptoJS from "crypto-js";
@@ -41,9 +41,37 @@ let blockchain: Block[] = [genesisBlock];
 
 const getBlockchain = () : Block[] => blockchain;
 
+// 이전 block을 가져오는 함수.
 const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
 
+// 새로운 Block 생성하는 함수로 리턴값은 Block임.
+const createNewBlock = (data:string) : Block => {
+    // 이전 block을 가져옴
+    const previosBlock : Block = getLatestBlock();
+    // 새로운 index 값을 설정
+    const newIndex : number = previosBlock.index + 1;
+    // 새로운 timestamp 값 설정
+    const newTimestamp : number = getNewTimeStamp();
+    // 새로운 Hash 값 설정
+    const newHash : string = Block.calculateBlockHash(
+        newIndex,
+        previosBlock.hash,
+        newTimestamp,
+        data
+    );
+    // 새로운 Block 생성
+    const newBlock : Block = new Block(
+        newIndex,
+        newHash,
+        previosBlock.hash,
+        data,
+        newTimestamp
+    );
+    return newBlock;
+};
+
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 
 export {};

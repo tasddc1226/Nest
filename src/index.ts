47e3f309761e24eeaa12a82e23897e075a2f8c68
tasddc1226 <tasddc@naver.com>
2021-07-23 22:59:47 +0900

    #4 Classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-// Typescript Part 3
-// Interfaces on Typescript
+// Typescript Part 4
+// Classes on Typescript
 
-// 인터페이스 선언
-interface Human {
-    name : string;
-    age : number;
-    gender : string;
+// ts의 클래스 선언에서는 어떤 속성들을 가져야 하는지 선언이 필요하다.
+// public 변수는 class 외부에서 호출 가능하지만
+// private 변수는 class 외부에서 호출이 불가능하다.
+class Human {
+    public name : string;
+    public age : number;
+    public gender : string;
+    constructor(name: string, age: number, gender: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "yang su young",
-    age: 25,
-    gender: "male"
-};
+const lynn = new Human("Lynn", 24, "female");
 
 const sayHi = (person : Human): string => {
     return (`Hi There, ${person.name}, you are ${person.age}, you are a ${person.gender}!`);
@@ -20,6 +23,6 @@ const sayHi = (person : Human): string => {
 
 // typescript가 person의 type을 Human 인터페이스와 같은지 확인함.
 // 인터페이스는 JS에서 작동을 하지 않는다.
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};

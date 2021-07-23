18523fea8a4e52399df2b1c75795a78e2eb508fb
tasddc1226 <tasddc@naver.com>
2021-07-23 23:11:53 +0900

    #5 Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,37 @@
-// Typescript Part 4
-// Classes on Typescript
+// Typescript Part 5
+// Blockchain Creating a Block
 
-// ts의 클래스 선언에서는 어떤 속성들을 가져야 하는지 선언이 필요하다.
-// public 변수는 class 외부에서 호출 가능하지만
-// private 변수는 class 외부에서 호출이 불가능하다.
-class Human {
-    public name : string;
-    public age : number;
-    public gender : string;
-    constructor(name: string, age: number, gender: string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const lynn = new Human("Lynn", 24, "female");
+const genesisBlock:Block = new Block(0, "2020202020", "", "Hello", 123456);
 
-const sayHi = (person : Human): string => {
-    return (`Hi There, ${person.name}, you are ${person.age}, you are a ${person.gender}!`);
-};
+// type이 Block인 것만 배열에 추가하도록 함.
+let blockchain: [Block] = [genesisBlock]; 
 
-// typescript가 person의 type을 Human 인터페이스와 같은지 확인함.
-// 인터페이스는 JS에서 작동을 하지 않는다.
-console.log(sayHi(lynn));
+// 따라서 아래는 push가 불가능 type이 Block이 아니기 때문!
+// blockchain.push("stuff");
+
+console.log(blockchain);
+
+// 많은 function과 많은 type이 존재할 경우 typescript를 사용하여 실수를 줄일 수 있다.
 
 export {};

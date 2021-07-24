dec20503589499de1d329825f5c51cc88783e877
tasddc1226 <tasddc@naver.com>
2021-07-25 02:01:48 +0900

    #1 setup empty module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-// controller의 역할
-// url을 가져오기 및 함수 실행
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

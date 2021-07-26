6a2aaf0703b7ef665c0d3a3a1ae5b6b687678c5d
tasddc1226 <tasddc@naver.com>
2021-07-27 00:02:35 +0900

    #2.3 Movies Service part Two

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -36,12 +36,7 @@ export class MoviesController {
     // 전체 movie를 update 시킴. @Put
     @Patch(':id')
     patch(@Param('id') movieId: string, @Body() updateData){ // 필요한 parameter를 직접 요청해야 사용 가능!
-        // 업데이트할 movie의 id랑 우리가 보낼 데이터의 Object를 return
-        return {
-            // json 형태로 리턴을 준다. express.js에서는 json으로 리턴하려면 설정을 해야하는데 nest에서는 불필요함.
-            updatedMovie: movieId,
-            ...updateData,
-        };
+        return this.moviesService.update(movieId, updateData);
     }
 
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,12 +10,19 @@ export class MoviesService {
     }
 
     getOne(id: string): Movie {
-        return this.movies.find(movie => movie.id === + id);
+        const movie = this.movies.find(movie => movie.id === + id);
+        // 없는 movieId의 번호를 요청받았다면?
+        if (!movie) {
+            // Nest에는 아래와 같은 편리한 기능이 있다.
+            throw new NotFoundException(`Movie with ID: ${id} not found.`)
+        }
+        return movie;
     }
 
-    deleteOne(id: string): boolean{
-        this.movies.filter(movie =>movie.id !== +id);
-        return true;
+    deleteOne(id: string){
+        this.getOne(id); // 해당 movieID 값이 존재하는지 확인
+        this.movies = this.movies.filter(movie => movie.id !== + id);
+        
     }
 
     create(movieData){
@@ -23,5 +30,13 @@ export class MoviesService {
             id: this.movies.length + 1,
             ...movieData,
         });
+    }
+
+    update(id:string, updateData){
+        // updateData의 유효성 검사가 필요함.
+        const movie = this.getOne(id);
+        this.deleteOne(id);
+        this.movies.push({...movie, ...updateData});
+        
     }
 }

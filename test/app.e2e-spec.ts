3c990c17cc46c4bd7f849d971be071b7e6a5d534
tasddc1226 <tasddc@naver.com>
2021-08-12 21:45:46 +0900

    #4.0 e2e Testing movies

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,30 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie api');
   });
+
+  // e2e test
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer())
+        .get('/movies')
+        .expect(200)
+        .expect([]);  // 빈 객체가 나와야 성공.
+    });
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['Test'],
+        })
+        .expect(201); // 201은 create 성공
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    })
+  });
+
 });

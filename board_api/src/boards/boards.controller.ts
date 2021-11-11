2d7fa29f651a14bc96e4ffc18a2f0c2c2fa3b4c7
tasddc1226 <tasddc@naver.com>
2021-11-11 22:36:12 +0900

    [feat] getBoardById

--- board_api/src/boards/boards.controller.ts
+++ board_api/src/boards/boards.controller.ts
@@ -1,20 +1,25 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Board } from './boards.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private BoardsService: BoardsService) { }
+    constructor(private boardsService: BoardsService) { }
 
     @Get()
     getAllboard(): Board[] {
-        return this.BoardsService.getAllBoards();
+        return this.boardsService.getAllBoards();
     }
 
     @Post()
     createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-        return this.BoardsService.createBoard(createBoardDto);
+        return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id);
     }
 
 }

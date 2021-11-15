3de321e15440f2d7964c87d1e5c55d5d61f23af9
tasddc1226 <tasddc@naver.com>
2021-11-15 22:57:07 +0900

    [feat] 커스텀 파이프 구현 및 Board Status의 readonly property 적용

---
+++ board_api/src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { BoardStatus } from "../boards.model";
+
+export class BoardStatusValidationPipe implements PipeTransform {
+
+    readonly StatusOptions = [
+        BoardStatus.PUBLIC,
+        BoardStatus.PRIVATE
+    ]
+
+    transform(value: any) {
+        value = value.toUpperCase();
+
+        if (!this.isStatusValid(value)) {
+            throw new BadRequestException(`${value} isn't in the status`);
+        }
+
+        return value;
+    }
+
+    private isStatusValid(status: any) {
+        const index = this.StatusOptions.indexOf(status);
+        return index !== -1;
+    }
+}

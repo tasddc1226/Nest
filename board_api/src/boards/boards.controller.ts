35325ef00cddf809225608b3cdb7739e542c2c6e
tasddc1226 <tasddc@naver.com>
2021-11-29 21:20:17 +0900

    [fix] repository를 사용해서 id로 board 가져오기

--- board_api/src/boards/boards.controller.ts
+++ board_api/src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -8,6 +9,11 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 export class BoardsController {
     constructor(private boardsService: BoardsService) {
 
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: number): Promise<Board> {
+        return this.boardsService.getBoardById(id);
     }
 
     // @Get()

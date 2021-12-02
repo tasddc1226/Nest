c5cf72d893211871ef99d06286f6cc1e9f6e10b6
tasddc1226 <tasddc@naver.com>
2021-12-02 16:21:10 +0900

    [feat] passport jwt 모듈 추가 및 login시 user Jwt token 생성

--- board_api/src/auth/auth.service.ts
+++ board_api/src/auth/auth.service.ts
@@ -3,26 +3,29 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from "bcryptjs";
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
         private userRepository: UserRepository,
+        private jwtService: JwtService
     ) { }
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.createUser(authCredentialsDto);
     }
 
-    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string>{
-        const {username, password} = authCredentialsDto;
-        const user = await this.userRepository.findOne({username});
-
-        if (user && (await bcrypt.compare(password, user.password))) {
-            return 'logIn success'
-        } else {
-            throw new UnauthorizedException('logIn failed')
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}>{
+        const username = await this.userRepository.signIn(authCredentialsDto);
+        if(!username) {
+            throw new UnauthorizedException('Invalid credentials');
         }
+        
+        const payload: JwtPayload = {username};
+        const accessToken = await this.jwtService.sign(payload);
+
+        return { accessToken };
     }
 }

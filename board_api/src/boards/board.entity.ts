cd1bcc3c039fb78752f1ff0ab9a2e21739be93a6
tasddc1226 <tasddc@naver.com>
2021-12-03 18:09:41 +0900

    [feat] User의 유효성 검증을 통해 board에 접근

--- board_api/src/boards/board.entity.ts
+++ board_api/src/boards/board.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/auth/user.entity";
+import { BaseEntity, Column, Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { BoardStatus } from "./board-status.enum";
 
 @Entity()
@@ -6,12 +7,15 @@ export class Board extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column("varchar")
+    @Column()
     title: string;
 
-    @Column("varchar")
+    @Column()
     description: string;
 
-    @Column("varchar")
+    @Column()
     status: BoardStatus;
+
+    @ManyToOne(type => User, user => user.boards, {eager: false})
+    user: User;
 }
--- board_api/src/boards/boards.controller.ts
+++ board_api/src/boards/boards.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
@@ -14,8 +16,10 @@ export class BoardsController {
     }
 
     @Get()
-    getAllBoard(): Promise<Board[]> {
-        return this.boardsService.getAllBoards();
+    getAllBoard(
+        @GetUser() user: User
+    ): Promise<Board[]> {
+        return this.boardsService.getAllBoards(user);
     }
 
     @Get('/:id')
@@ -25,13 +29,19 @@ export class BoardsController {
 
     @Post()
     @UsePipes(ValidationPipe)
-    createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
-        return this.boardsService.createBoard(createBoardDto);
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto,
+        @GetUser() user: User,
+    ): Promise<Board> {
+        return this.boardsService.createBoard(createBoardDto, user);
     }
 
     @Delete('/:id')
-    deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
-        return this.boardsService.deleteBoard(id);
+    deleteBoard(
+        @Param('id', ParseIntPipe) id: number,
+        @GetUser() user: User
+    ): Promise<void> {
+        return this.boardsService.deleteBoard(id, user);
     }
 
     @Patch('/:id/status')

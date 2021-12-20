2e4e80f95e384c709af0db1f15b7ae61e8feeacc
tasddc1226 <tasddc@naver.com>
2021-12-20 21:28:16 +0900

    [add] mongoose 연결 및 User Schema 생성

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://tasddc:dan9797@bolierplate.xxf1w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useCreateIndex: true,   몽구스 버전이 6.0 이상이면 error 발생
+      //useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected.."))
+  .catch((err) => console.log(err));
 
-app.listen(port, () => console.log(`listening on port ${port}!`))
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`listening on port ${port}!`));

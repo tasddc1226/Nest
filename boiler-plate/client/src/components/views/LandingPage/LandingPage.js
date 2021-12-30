b84a927043a0c6b3a94a167beabccf068b2d14b3
tasddc1226 <tasddc@naver.com>
2021-12-30 12:03:40 +0900

    [feat] logout 기능 구현

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,24 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function LandingPage() {
+  let navigate = useNavigate();
+
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then(response => {
+      if(response.data.success) {
+        alert('로그아웃 성공! 로그인 페이지로 이동합니다.');
+        navigate("/login");
+      } else {
+        alert('로그아웃 실패!');
+      }
+    })
+  }
 
   return (
     <div
@@ -17,6 +31,8 @@ function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }

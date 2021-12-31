4eac33a6c1f537a76b486e214a2baf3724e810d1
tasddc1226 <tasddc@naver.com>
2021-12-31 13:45:36 +0900

    [feat] GridCard 컴포넌트 추가

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Button } from 'antd';
-import { withRouter } from 'react-router-dom';
+import { Row } from 'antd';
 import { API_KEY, API_URL, IMAGE_BASE_URL } from "../../../Config";
 import MainImage from "./Sections/MainImage";
+import GridCards from "../commons/GridCards";
 
 function LandingPage(props) {
 
@@ -11,12 +11,13 @@ function LandingPage(props) {
   const [MainMovieImage, setMainMovieImage] = useState(null)
 
   useEffect(() => {
-    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=kr-KR&page=1`;
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
     fetch(endpoint)
     .then(response => response.json())
     .then(response => {
-      setMovies([response.results])
+      // console.log(response)
+      setMovies([...Movies, ...response.results])
       setMainMovieImage(response.results[0])
     })
 
@@ -43,6 +44,18 @@ function LandingPage(props) {
         <hr/>
 
         {/* Movie Grid Cards */}
+        <Row gutter={[16, 16]}>
+          {Movies && Movies.map((movie, index) => (
+            <React.Fragment key={index}>
+              <GridCards 
+                image={movie.poster_path ?
+                  `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                  movieId={movie.id}
+                  movieName={movie.original_title}
+              />
+            </React.Fragment>
+          ))}
+        </Row>
 
       </div>
 

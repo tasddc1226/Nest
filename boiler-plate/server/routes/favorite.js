8d0b1eb3e385ad3630e551b03ba0dcce18fc4b60
tasddc1226 <tasddc@naver.com>
2022-01-06 17:43:52 +0900

    [feat] favorite page에서 좋아요한 영화 list에서 삭제 구현

--- boiler-plate/server/routes/favorite.js
+++ boiler-plate/server/routes/favorite.js
@@ -1,57 +1,57 @@
-const express = require("express");
-const router = express.Router();
-const { Favorite } = require("../models/Favorite");
+const express = require('express')
+const router = express.Router()
+const { Favorite } = require('../models/Favorite')
 
-router.post("/favoriteNumber", (req, res) => {
+router.post('/favoriteNumber', (req, res) => {
   // DB에서 favorite 숫자 가져오기
   Favorite.find({ movieId: req.body.movieId }).exec((err, info) => {
-    if (err) return res.status(400).send(err);
+    if (err) return res.status(400).send(err)
 
     // front로 숫자 정보 보내기
-    res.status(200).json({ success: true, favoriteNumber: info.length });
-  });
-});
+    res.status(200).json({ success: true, favoriteNumber: info.length })
+  })
+})
 
-router.post("/favorited", (req, res) => {
+router.post('/favorited', (req, res) => {
   // 내가 Favorite 리스트에 넣었는지 정보를 DB에서 가져오기
   Favorite.find({
     movieId: req.body.movieId,
     userFrom: req.body.userFrom,
   }).exec((err, info) => {
-    if (err) return res.status(400).send(err);
+    if (err) return res.status(400).send(err)
 
-    let result = false;
+    let result = false
     if (info.length !== 0) {
-      result = true;
+      result = true
     }
 
-    res.status(200).json({ success: true, favorited: result });
-  });
-});
+    res.status(200).json({ success: true, favorited: result })
+  })
+})
 
-router.post("/removeFromFavorite", (req, res) => {
+router.post('/removeFromFavorite', (req, res) => {
   Favorite.findOneAndDelete({
     movieId: req.body.movieId,
     userFrom: req.body.userFrom,
   }).exec((err, doc) => {
-    if (err) return res.status(400).send(err);
-    res.status(200).json({ success: true, doc });
-  });
-});
+    if (err) return res.status(400).send(err)
+    return res.status(200).json({ success: true, doc })
+  })
+})
 
-router.post("/addToFavorite", (req, res) => {
-  const favorite = new Favorite(req.body);
+router.post('/addToFavorite', (req, res) => {
+  const favorite = new Favorite(req.body)
   favorite.save((err, doc) => {
-    if (err) return res.status(400).send(err);
-    return res.status(200).json({ success: true });
-  });
-});
+    if (err) return res.status(400).send(err)
+    return res.status(200).json({ success: true })
+  })
+})
 
-router.post("/getFavoritedMovie", (req, res) => {
+router.post('/getFavoritedMovie', (req, res) => {
   Favorite.find({ userFrom: req.body.userFrom }).exec((err, favorites) => {
-    if (err) return res.status(400).send(err);
-    return res.status(200).json({ success: true, favorites });
-  });
-});
+    if (err) return res.status(400).send(err)
+    return res.status(200).json({ success: true, favorites })
+  })
+})
 
-module.exports = router;
+module.exports = router

f4ff0e2bce9366ad5e20d20b987e9bb7883de82b
tasddc1226 <tasddc@naver.com>
2022-01-07 10:53:26 +0900

    [fix] 댓글 목록을 가져오는 map func의 key값 수정

--- boiler-plate/client/src/components/views/MovieDetail/Sections/Comment.js
+++ boiler-plate/client/src/components/views/MovieDetail/Sections/Comment.js
@@ -1,36 +1,36 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import SingleComment from "./SingleComment";
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import SingleComment from './SingleComment'
 
 function Comment(props) {
   //   const movieId = props.movieId;
-  const user = useSelector((state) => state.user);
-  const [commentValue, setcommentValue] = useState("");
+  const user = useSelector((state) => state.user)
+  const [commentValue, setcommentValue] = useState('')
   const handleClick = (event) => {
-    setcommentValue(event.currentTarget.value);
-  };
+    setcommentValue(event.currentTarget.value)
+  }
 
   const onSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     const variables = {
       content: commentValue,
       writer: user.userData._id,
       movieId: props.movieId,
-    };
+    }
 
-    axios.post("/api/comment/saveComment", variables).then((response) => {
+    axios.post('/api/comment/saveComment', variables).then((response) => {
       if (response.data.success) {
-        console.log(response.data.result);
+        console.log(response.data.result)
         // MovieDetail의 Comments를 update
-        props.refreshFunction(response.data.result);
-        setcommentValue("");
+        props.refreshFunction(response.data.result)
+        setcommentValue('')
       } else {
-        alert("댓글을 저장하는데 실패했습니다..!");
+        alert('댓글을 저장하는데 실패했습니다..!')
       }
-    });
-  };
+    })
+  }
 
   return (
     <div>
@@ -44,29 +44,30 @@ function Comment(props) {
           (comment, index) =>
             !comment.responseTo && (
               <SingleComment
+                movieId={props.movieId}
                 refreshFunction={props.refreshFunction}
                 comment={comment}
-                movieId={props.movieId}
+                key={index}
               />
-            )
+            ),
         )}
 
       {/* Root Comment Form */}
 
-      <form style={{ display: "flex" }} onSubmit={onSubmit}>
+      <form style={{ display: 'flex' }} onSubmit={onSubmit}>
         <textarea
-          style={{ width: "100%", borderRadius: "5px" }}
+          style={{ width: '100%', borderRadius: '5px' }}
           onChange={handleClick}
           value={commentValue}
           placeholder="영화에 대한 댓글을 달아주세요."
         />
         <br />
-        <button style={{ width: "20%", height: "52px" }} onClick={onSubmit}>
+        <button style={{ width: '20%', height: '52px' }} onClick={onSubmit}>
           Submit
         </button>
       </form>
     </div>
-  );
+  )
 }
 
-export default Comment;
+export default Comment

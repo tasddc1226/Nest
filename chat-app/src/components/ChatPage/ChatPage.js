81b97cbe3ae839db5affac8f0b8bd0aa1062845e
tasddc1226 <tasddc@naver.com>
2022-01-26 09:01:24 +0900

    [add] Chat Page 컴포넌트 배치

--- chat-app/src/components/ChatPage/ChatPage.js
+++ chat-app/src/components/ChatPage/ChatPage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import SidePanel from "./SidePanel/SidePanel";
+import MainPanel from "./MainPanel/MainPanel";
 
 function ChatPage() {
-  return <div>ChatPage</div>;
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ width: "300px" }}>
+        <SidePanel />
+      </div>
+      <div style={{ width: "100%" }}>
+        {/* <MainPanel key={currentChatRoom && currentChatRoom.id} /> */}
+        <MainPanel />
+      </div>
+    </div>
+  );
 }
 
 export default ChatPage;

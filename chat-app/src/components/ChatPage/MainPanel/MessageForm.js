771bf7718d1f4c1f507f1595157c9418d3fb105b
tasddc1226 <tasddc@naver.com>
2022-01-26 15:24:14 +0900

    [feat] 입력한 채팅 firebase 저장

--- chat-app/src/components/ChatPage/MainPanel/MessageForm.js
+++ chat-app/src/components/ChatPage/MainPanel/MessageForm.js
@@ -6,13 +6,71 @@ import Col from "react-bootstrap/Col";
 import firebase from "../../../firebase";
 import { useSelector } from "react-redux";
 import mime from "mime-types";
+
 function MessageForm() {
+  // 현재 자신이 속해있는 채널의 정보를 가져옴
+  const chatRoom = useSelector((state) => state.chatRoom.currentChatRoom);
+  const user = useSelector((state) => state.user.currentUser);
+  const [content, setContent] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const messagesRef = firebase.database().ref("messages");
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const createMessage = (fileUrl = null) => {
+    const message = {
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      user: {
+        id: user.uid,
+        name: user.displayName,
+        image: user.photoURL,
+      },
+    };
+
+    if (fileUrl != null) {
+      message["image"] = fileUrl;
+    } else {
+      message["content"] = content;
+    }
+    return message;
+  };
+
+  const handleSubmit = async () => {
+    if (!content) {
+      setErrors((prev) => prev.concat("Type contents first"));
+      return;
+    }
+    setLoading(true);
+
+    // firebase에 data를 저장하는 부분
+    try {
+      await messagesRef.child(chatRoom.id).push().set(createMessage());
+      setLoading(false);
+      setContent("");
+      setErrors([]);
+    } catch (error) {
+      setErrors((prev) => prev.concat(error.message));
+      setLoading(false);
+      setTimeout(() => {
+        setErrors([]);
+      }, 5000);
+    }
+  };
+
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control
+            value={content}
+            onChange={handleChange}
+            as="textarea"
+            rows={2}
+          />
         </Form.Group>
       </Form>
 
@@ -24,10 +82,18 @@ function MessageForm() {
         // now={percentage}
       />
 
+      <div>
+        {errors.map((errorMsg) => (
+          <p style={{ color: "red" }} key={errorMsg}>
+            {errorMsg}
+          </p>
+        ))}
+      </div>
+
       <Row>
         <Col>
           <button
-            // onClick={handleSubmit}
+            onClick={handleSubmit}
             className="message-form-button"
             style={{ width: "100%" }}
             // disabled={loading ? true : false}

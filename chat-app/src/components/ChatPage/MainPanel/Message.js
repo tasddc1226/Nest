097fec01d18a8282100e1340ffb5dde04b7b176b
tasddc1226 <tasddc@naver.com>
2022-01-26 16:22:32 +0900

    [feat] 실시간 대화 render 및 내용 띄우기

--- chat-app/src/components/ChatPage/MainPanel/Message.js
+++ chat-app/src/components/ChatPage/MainPanel/Message.js
@@ -1,7 +1,49 @@
 import React from "react";
+import Media from "react-bootstrap/Media";
+import moment from "moment";
 
-function Message() {
-  return <div>Message</div>;
+function Message({ message, user }) {
+  const timeFromNow = (timestamp) => moment(timestamp).fromNow();
+
+  const isImage = (message) => {
+    return (
+      message.hasOwnProperty("image") && !message.hasOwnProperty("content")
+    );
+  };
+
+  const isMessageMine = (message, user) => {
+    return message.user.id === user.uid;
+  };
+
+  return (
+    <Media style={{ marginBottom: "3px" }}>
+      <img
+        style={{ borderRadius: "15px" }}
+        width={48}
+        height={48}
+        className="mr-3"
+        src={message.user.image}
+        alt={message.user.name}
+      />
+      <Media.Body
+        style={{
+          backgroundColor: isMessageMine(message, user) && "#ECECEC",
+        }}
+      >
+        <h6>
+          {message.user.name}{" "}
+          <span style={{ fontSize: "7px", color: "gray" }}>
+            {timeFromNow(message.timestamp)}
+          </span>
+        </h6>
+        {isImage(message) ? (
+          <img style={{ maxWidth: "300px" }} alt="이미지" src={message.image} />
+        ) : (
+          <p>{message.content}</p>
+        )}
+      </Media.Body>
+    </Media>
+  );
 }
 
 export default Message;

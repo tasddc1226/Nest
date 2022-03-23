0420738cbb5351eb6829268e10e1a8852465c5d9
tasddc1226 <tasddc@naver.com>
2022-03-23 13:17:12 +0900

    [add] gateway의 생명주기

--- Nest-DeepDive/4. 랜덤 채팅 웹 앱/src/chats/chats.gateway.ts
+++ Nest-DeepDive/4. 랜덤 채팅 웹 앱/src/chats/chats.gateway.ts
@@ -1,13 +1,40 @@
+import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
-import { Socket } from 'dgram';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: 'chattings' })
-export class ChatsGateway {
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  afterInit() {
+    // constructor 이후 실행되어질 logic
+    this.logger.log('init');
+  }
+
+  // clien와 connection이 발생하면 실행하는 함수
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`new user connected : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  // client와 connection이 끊기면 실행하는 함수
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`user disconnected : ${socket.id} ${socket.nsp.name}`);
+  }
+
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
